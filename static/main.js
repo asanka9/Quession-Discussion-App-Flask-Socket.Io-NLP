0e9936e5c93f0709461e2abaf8a1a5c08464c212
asanka9 <sendasanka96@gmail.com>
2020-11-21 10:52:52 +0530

    wwew

--- static/main.js
+++ static/main.js
@@ -30,8 +30,8 @@ user_form.classList.remove('hidden');
 instructor_form.classList.add('hidden');
 
 
-var socket = io.connect('https://nlpdep.herokuapp.com:5000');
-var private_socket = io('https://nlpdep.herokuapp.com:5000/private');
+var socket = io.connect('https://nlpqwe231ddep.herokuapp.com:5000');
+var private_socket = io('https://nlpqwe231ddep.herokuapp.com:5000/private');
 
 
 $(document).ready(function() {
